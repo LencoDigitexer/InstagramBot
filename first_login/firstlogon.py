edee5b5f1fcaa8049d8603a98843dbb5d02be7a2
LencoDigitexer <44779327+LencoDigitexer@users.noreply.github.com>
2019-10-10 20:06:43 +0300

    Update firstlogon.py

--- first_login/firstlogon.py
+++ first_login/firstlogon.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options #для сохранения куки в первый раз
 import os 
 import time
-import sys
+from data import login, password
 
 #---подключение драйвера chrome в текущей директории и настройка куки
 chrome_options = Options()
@@ -12,16 +12,14 @@ path= str(os.getcwd()) + r"\chromedriver.exe"
 driver = webdriver.Chrome(executable_path=path, options=chrome_options)
 chrome_options.add_argument("user-data-dir=chrome-data") 
 
-#---логин с паролем
-login = sys.argv[1]
-passwd = sys.argv[2]
-
 #---заходим в инстаграм с...
 driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-elem_login = driver.find_element_by_css_selector("#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input")#---... логином ...
+time.sleep(3)
+#elem_login = driver.find_element_by_name("username") #---... логином ...
+elem_login = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input')
 elem_login.send_keys(login)
-elem_passwd = driver.find_element_by_css_selector("#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(3) > div > label > input")#---... и паролем.
-elem_passwd.send_keys(passwd)
+elem_passwd = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input')
+elem_passwd.send_keys(password)
 elem_passwd.send_keys(Keys.RETURN)
 time.sleep(50)
 driver.close()
