3e7d8a34b9218e7c83728d5af64efa01ee98dfe7
LencoDigitexer <44779327+LencoDigitexer@users.noreply.github.com>
2019-10-10 19:25:43 +0300

    Update firstlogon.py

--- first_login/firstlogon.py
+++ first_login/firstlogon.py
@@ -18,11 +18,10 @@ passwd = sys.argv[2]
 
 #---заходим в инстаграм с...
 driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-elem_login = driver.find_element_by_name("username") #---... логином ...
+elem_login = driver.find_element_by_css_selector("#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input")#---... логином ...
 elem_login.send_keys(login)
-elem_passwd = driver.find_element_by_name("password")#---... и паролем.
+elem_passwd = driver.find_element_by_css_selector("#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(3) > div > label > input")#---... и паролем.
 elem_passwd.send_keys(passwd)
 elem_passwd.send_keys(Keys.RETURN)
 time.sleep(50)
-
-
+driver.close()
